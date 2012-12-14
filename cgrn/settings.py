596ea2e197930e7a59c7ea1c92ebaa4054462600
Jamie Neil <jamie@goron>
2012-12-14 19:29:57 +0100

    Static files

--- cgrn/settings.py
+++ cgrn/settings.py
@@ -13,8 +13,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'cgrn',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'USER': 'cgrn',                      # Not used with sqlite3.
+        'PASSWORD': 'cgrn',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/var/www/cgrn/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
